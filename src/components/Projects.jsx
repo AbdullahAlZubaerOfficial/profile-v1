d20372d8a7aa87b5fba894ddf4f71dbd050039ea
Zubaer3002 <your-email@example.com>
2025-07-31 18:46:53 +0600

    up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,117 @@
-// projects.js (example)
+// projects.js
 export const projects = [
   {
     id: "1",
-    name: "DocConnect",
-    description: "DocConnect is a modern healthcare application designed to connect patients with doctors seamlessly. This app allows users to browse doctor profiles, book appointments, and communicate directly with healthcare professionals through chat or video calls. It also provides features like prescription management, health records tracking, and real-time notifications for appointment reminders. DocConnect aims to make healthcare accessible, efficient, and convenient for everyone, anytime, anywhere. 🌍💙",
-    image: "https://uicookies.com/wp-content/uploads/2019/08/drcare-free-doctor-website-templates.jpg"
-    ,
+    name: "DocConnect 🩺",
+    description:
+      "DocConnect is a modern healthcare application designed to connect patients with doctors seamlessly. This app allows users to browse doctor profiles, book appointments, and communicate directly with healthcare professionals through chat or video calls. It also provides features like prescription management, health records tracking, and real-time notifications for appointment reminders. DocConnect aims to make healthcare accessible, efficient, and convenient for everyone, anytime, anywhere. 🌍💙",
+    image:
+      "https://uicookies.com/wp-content/uploads/2019/08/drcare-free-doctor-website-templates.jpg",
     liveUrl: "https://doctor-client-new.vercel.app/",
     clientRepo: "https://github.com/AbdullahAlZubaerOfficial/Doctor-Client-New",
     serverRepo: "https://github.com/AbdullahAlZubaerOfficial/doctor-server",
-    tags: ["React", "Tailwind","Node.js", "Express.js", "Mongodb", "JWT","Firebase","Tanstack"],
+    tags: [
+      "React",
+      "Tailwind",
+      "Node.js",
+      "Express.js",
+      "Mongodb",
+      "JWT",
+      "Firebase",
+      "Tanstack",
+    ],
     category: "Web Development",
   },
-  // onno project objects
-
-    {
+  {
     id: "2",
     name: "RealTimeChatPro 💬",
-    description: "RealTimeChatPro is a high-performance real-time chat application built with React, Next.js, Node.js, Socket.IO, and MongoDB. It enables instant messaging between users with real-time updates, delivering smooth and seamless conversations. The app supports features like private messaging, group chats, online/offline status indicators, typing indicators, and message history storage. With the power of Socket.IO for bi-directional communication and MongoDB for scalable data storage, RealTimeChatPro provides a robust and responsive chat experience perfect for modern web applications. 🚀💬",
-    image: "https://i.ibb.co/MkrYs9XT/Screenshot-2025-07-28-002211.png"
-    ,
+    description:
+      "RealTimeChatPro is a high-performance real-time chat application built with React, Next.js, Node.js, Socket.IO, and MongoDB. It enables instant messaging between users with real-time updates, delivering smooth and seamless conversations. The app supports features like private messaging, group chats, online/offline status indicators, typing indicators, and message history storage. With the power of Socket.IO for bi-directional communication and MongoDB for scalable data storage, RealTimeChatPro provides a robust and responsive chat experience perfect for modern web applications. 🚀💬",
+    image: "https://i.ibb.co/MkrYs9XT/Screenshot-2025-07-28-002211.png",
     liveUrl: "https://real-time-chat-app-pi-five.vercel.app/",
-    clientRepo: "https://github.com/AbdullahAlZubaerOfficial/Real-Time-Chat-App/tree/main/client",
-    serverRepo: "https://github.com/AbdullahAlZubaerOfficial/Real-Time-Chat-App/tree/main/server",
-    tags: ["React", "Socket.io",  "Node.js", "Express.js", "Mongodb", "JWT", "Tailwind",],
+    clientRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/Real-Time-Chat-App/tree/main/client",
+    serverRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/Real-Time-Chat-App/tree/main/server",
+    tags: [
+      "React",
+      "Socket.io",
+      "Node.js",
+      "Express.js",
+      "Mongodb",
+      "JWT",
+      "Tailwind",
+    ],
     category: "Web Development",
+  },
+  {
+    id: "3",
+    name: "FoodFiesta 🍔",
+    description:
+      "FoodFiesta is a full-stack food ordering web application that enables users to browse food items, place orders, and complete payments online. After successful transactions, a unique Transaction ID is generated and securely saved in the Order History for user reference.\n\nThe platform includes a fully functional Admin Dashboard that allows administrators to:\n\n- Monitor total revenue with interactive graphical analytics\n- Add, update, or delete food items\n- Manage users and view all registered accounts\n- Oversee the entire platform seamlessly\n\n💡 Key Highlights:\n- Stripe payment integration with transaction tracking\n- Firebase Authentication for secure user login\n- Dynamic cart and order system\n- Responsive UI with React, Tailwind CSS & DaisyUI\n- MongoDB for robust and scalable data management\n- Revenue visualization using Recharts\n\nThis project reflects real-world e-commerce and admin panel functionality, making it ideal for showcasing full-stack development skills.",
+    image: "https://i.ibb.co/4ZMLYKdp/Screenshot-2025-07-31-163706.png",
+    liveUrl: "https://bistro-boss-f9cc6.web.app/",
+    clientRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/bistro-boss-client",
+    serverRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/bistro-boss-server",
+    tags: [
+      "React",
+      "Tailwind",
+      "DaisyUI",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Stripe",
+      "JWT",
+      "Firebase",
+      "Recharts",
+    ],
+    category: "Web Development",
+  },
+  {
+    id: "4",
+    name: "Learning Management System",
+    description:
+      "LMS is a full-stack Learning Management System designed to provide an efficient, scalable, and engaging online learning experience. It allows administrators to manage courses and users, instructors to upload content, and students to access learning materials, track progress, and interact with instructors.",
+    image: "https://i.ibb.co/YFCh4hp0/Screenshot-2025-07-31-173112.png",
+    liveUrl: "https://lms-iwqx.vercel.app/",
+    clientRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/lms/tree/main/client",
+    serverRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/lms/tree/main/server",
+    tags: [
+      "React",
+      "Socket.io",
+      "Node.js",
+      "Express.js",
+      "Mongodb",
+      "JWT",
+      "Tailwind",
+    ],
+    category: "Web Development",
+  },
+  {
+    id: "5",
+    name: "Competetive Programming Sheet",
+    description:
+      "This repository is thoughtfully crafted for learners and passionate programmers who aim to strengthen their grasp on Data Structures, Algorithms, and Math-based Competitive Programming problems.  It offers a wide range of important and frequently asked problems, accompanied by optimized and efficient solutions that follow standard competitive programming (CP) practices. Whether you're just starting out or already at an intermediate level, this resource will guide you through the essential concepts and help you develop a strong foundation using C and C++. 💪 You’ll also find well-organized practice sheets designed for regular use, embedded with performance tips to help you write faster and cleaner code. This repository is more than just a collection of problems — it’s your daily coding partner in the journey of becoming a better programmer. 🔥",
+    image: "https://i.ibb.co/wZBVLZ1f/1750918710618.jpg",
+    liveUrl: "https://github.com/AbdullahAlZubaerOfficial/CP",
+    clientRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/CP",
+    serverRepo:
+      "https://github.com/AbdullahAlZubaerOfficial/CP",
+    tags: [
+       "C",
+       "C++",
+       "LinkedList",
+       "Codeforce Problem Sheet Solve ",
+       "Binary Tree",
+       "Stack",
+       "Queue",
+       "Graph"
+    ],
+    category: "",
   },
 ];
